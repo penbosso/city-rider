1392e6bc7611b6838198b39bde1a0aa0635eb5db
penbosso <ckbosso@st.ug.edu.gh>
2020-08-28 20:28:07 +0000

    feat: delivery and feedback APIs

---
+++ README.md
@@ -0,0 +1,3 @@
+# City Rider Delivery / Feed back endpoint
+
+City Rider Delivery / Feed back endpoint
---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '4feecac0140469d5f446e2378c6e2111'),
+    },
+  },
+});
