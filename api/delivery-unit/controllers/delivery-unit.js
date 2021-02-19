dbba8e9d13ecc83df3bd64bedeaaaf11037b80fc
penbosso <ckbosso@st.ug.edu.gh>
2021-02-19 20:33:52 +0000

    woking on active delivery unit

--- api/delivery-unit/controllers/delivery-unit.js
+++ api/delivery-unit/controllers/delivery-unit.js
@@ -7,6 +7,23 @@ const { sanitizeEntity } = require('strapi-utils');
  */
 
 module.exports = {
+
+  /**
+   * delivery unittes that are not yet delivered or cancelled.
+   *
+   * @return {Object}
+   */
+  active: async ctx => {
+    // use the current user id from the JWT in the header
+    const decrypted = await strapi.plugins[
+      'users-permissions'
+    ].services.jwt.getToken(ctx);
+
+console.log(decrypted._doc._id,'------------------------------------------');
+    let entities = await strapi.services['delivery-unit'].find({ status_nin: ['cancelled', 'delivered'] });
+
+    return entities.map(entity => sanitizeEntity(entity, { model: strapi.models['delivery-unit'] }));
+  },
   /**
    * Create a record.
    *
@@ -14,6 +31,19 @@ module.exports = {
    */
 
   async create(ctx) {
+    // use the current user id from the JWT in the header
+    const decrypted = await strapi.plugins[
+      'users-permissions'
+    ].services.jwt.getToken(ctx);
+    ctx.request.body.user = decrypted._doc._id;
+
+    if(ctx.request.body.matrix) {
+      const duration = ctx.request.body.matrix.durations[0][0] + ctx.request.body.matrix.durations[0][1];
+      const distances = ctx.request.body.matrix.durations[0][0] + ctx.request.body.matrix.distances[0][1];
+
+      ctx.request.body.estimatedTime = duration;
+      ctx.request.body.distance = distances;
+    }
     ctx.request.body.distance? '' : ctx.request.body.distance = 10;
     ctx.request.body.estimatedTime? '' : ctx.request.body.estimatedTime = 20;
     ctx.request.body.cost = calculateCost(ctx.request.body.distance,ctx.request.body.estimatedTime);
@@ -23,4 +53,5 @@ module.exports = {
   },
 };
 
-const calculateCost = (distance, estimatedTime) => 20 + 0.1 * distance + 0.15 * estimatedTime
+// one km is equivalent to 1 ghc, 100 unit of duration is to 1 ghc
+const calculateCost = (distance, estimatedTime) => 20 + 0.1 * distance + 0.01 * estimatedTime
