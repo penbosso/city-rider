0479cdfedd056033940b266b0093e8daded4c85c
penbosso <ckbosso@st.ug.edu.gh>
2021-03-15 00:18:46 +0000

    Settings API imp

--- api/delivery-unit/controllers/delivery-unit.js
+++ api/delivery-unit/controllers/delivery-unit.js
@@ -21,7 +21,7 @@ module.exports = {
 
     let entities = await strapi.services['delivery-unit'].find({user:decrypted._doc._id, status_nin: ['cancelled', 'delivered'] });
 
-    return entities.map(entity => sanitizeEntity(entity, { model: strapi.models['delivery-unit'] }));
+    return entities.map(entity => sanitizeEntity(entity, { model: strapi.models['delivery-unit'] })).reverse();
   },
 
   async statistics() {
@@ -87,17 +87,14 @@ module.exports = {
     ].services.jwt.getToken(ctx);
     ctx.request.body.user = decrypted._doc._id;
 
-    if(ctx.request.body.matrixBug) {
-      const duration = ctx.request.body.matrix.durations[0][0] + ctx.request.body.matrix.durations[0][1];
-      const distances = ctx.request.body.matrix.durations[0][0] + ctx.request.body.matrix.distances[0][1];
-
-      ctx.request.body.estimatedTime = duration;
-      ctx.request.body.distance = distances;
+    let distance = 10
+    if(ctx.request.body.matrix) {
+      distance = parseFloat(ctx.request.body.matrix.destinations[0]["snapped_distance"]);
     }
+
     ctx.request.body.status = "pending";
-    ctx.request.body.distance? '' : ctx.request.body.distance = 10;
-    ctx.request.body.estimatedTime? '' : ctx.request.body.estimatedTime = 20;
-    ctx.request.body.cost = calculateCost(ctx.request.body.distance,ctx.request.body.estimatedTime);
+    const settings = await strapi.services.setting.findOne({ id:"604e9d1cc687f574e8e1c3c5" });
+    ctx.request.body.cost = calculateCost(distance, settings.pricePerKm, settings.basePrice);
     let entity = await strapi.services['delivery-unit'].create(ctx.request.body);
 
     return sanitizeEntity({_id: entity._id, cost: entity.cost}, { model: strapi.models['delivery-unit'] });
@@ -105,4 +102,4 @@ module.exports = {
 };
 
 // one km is equivalent to 1 ghc, 100 unit of duration is to 1 ghc
-const calculateCost = (distance, estimatedTime) => 20 + 0.1 * distance + 0.01 * estimatedTime
+const calculateCost = (distance, pricePerKm, basePrice) => Math.ceil(basePrice + pricePerKm * distance)
