3e9a031b33299d85ec1687322e8619d554c9b1fa
penbosso <ckbosso@st.ug.edu.gh>
2021-06-05 23:05:17 +0000

    add limit 1kkk to log find

--- api/log/controllers/log.js
+++ api/log/controllers/log.js
@@ -1,8 +1,13 @@
 'use strict';
-
+const { sanitizeEntity } = require('strapi-utils');
 /**
  * Read the documentation (https://strapi.io/documentation/v3.x/concepts/controllers.html#core-controllers)
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+  async find(ctx) {
+    const entity = await strapi.services.log.find({ _limit: 1000000000000 });
+    return sanitizeEntity(entity, { model: strapi.models.log });
+  },
+};
